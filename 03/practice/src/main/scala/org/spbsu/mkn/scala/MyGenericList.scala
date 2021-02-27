05ceba26556cadf36cbe4f75befd38b1d547c2bc
IdeaSeeker <stroganov.n.36@gmail.com>
2021-02-27 13:17:55 +0300

    + MyGenericList

--- 03/practice/src/main/scala/org/spbsu/mkn/scala/MyGenericList.scala
+++ 03/practice/src/main/scala/org/spbsu/mkn/scala/MyGenericList.scala
@@ -2,19 +2,68 @@ package org.spbsu.mkn.scala
 
 import org.spbsu.mkn.scala.MyGenericList._
 
-sealed trait MyGenericList {
-  def head: Int
-  def tail: MyGenericList
-  def drop(n: Int): MyGenericList
-  def take(n: Int): MyGenericList
-  def map(f: Int => Int): MyGenericList
-  def ::(elem: Int): MyGenericList = ???
+sealed trait MyGenericList[+T] {
+    def head: T
+    def tail: MyGenericList[T]
+    def drop(n: Int): MyGenericList[T]
+    def take(n: Int): MyGenericList[T]
+    def map[U](f: T => U): MyGenericList[U]
+    def ::[U >: T](head: U): MyGenericList[U] = MyCons(head, this)
+    def foldLeft[S](init: S)(f: (S, T) => S): S
+}
+
+case object MyNil extends MyGenericList[Nothing] {
+    override def head: Nothing = undef
+
+    override def tail: Nothing = undef
+
+    override def drop(n: Int): MyGenericList[Nothing] = n match {
+        case 0 => MyNil
+        case _ => undef
+    }
+
+    override def take(n: Int): MyGenericList[Nothing] = n match {
+        case 0 => MyNil
+        case _ => undef
+    }
+
+    override def map[U](f: Nothing => U): MyGenericList[U] = MyNil
+
+    override def foldLeft[S](init: S)(f: (S, Nothing) => S): S = init
+}
+
+case class MyCons[T](override val head: T,
+                     override val tail: MyGenericList[T]) extends MyGenericList[T] {
+
+    override def drop(n: Int): MyGenericList[T] = n match {
+        case 0 => this
+        case n => tail.drop(n - 1)
+    }
+
+    override def take(n: Int): MyGenericList[T] = n match {
+        case 0 => MyNil
+        case n => MyCons(head, tail.take(n - 1))
+    }
+
+    override def map[U](f: T => U): MyGenericList[U] =
+        MyCons(f(head), tail.map(f))
+
+    override def foldLeft[S](init: S)(f: (S, T) => S): S =
+        tail.foldLeft(f(init, head))(f)
 }
 
 object MyGenericList {
-  def undef: Nothing = throw new UnsupportedOperationException("operation is undefined")
-  def fromSeq(seq: Seq[Int]): MyGenericList = ???
-  def size(intList: MyGenericList): Int     = ???
-  // extra task: implement sum using foldLeft
-  // def foldLeft(???)(???): ??? = ???
+    def undef: Nothing =
+        throw new UnsupportedOperationException("operation is undefined")
+
+    def fromSeq[T](seq: Seq[T]): MyGenericList[T] =
+        seq.foldRight(MyNil: MyGenericList[T])((head, tail) => head :: tail)
+
+    def sum(myList: MyGenericList[Int]): Int = myList match {
+        case MyNil  => undef
+        case myList => myList.foldLeft(0 : Int)((a, b) => a + b)
+    }
+
+    def size(myList: MyGenericList[_]): Int =
+        myList.foldLeft(0)((n, _) => n + 1)
 }
